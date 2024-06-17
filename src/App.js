cf13aa223b3839c503dde7be5f0780b4d90574f7
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-06-17 16:55:17 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css'
+export default function App() {
+    return (
+    <div>
+    <section>
+      <div className='div1'>
+        <h1 className='titulo'>Olá, eu sou kleberson</h1>
+        <p className='texto'>Nascido em 02/05/2007, em Brasilia de Minas-MG. sou um novo estudante na ELITI, uma escola excelente de programadores, ainda sou novato com progração, tendo recentemente completado um mês na ELITI. ainda sou meio inexperiente, tendo começado agora no react, mas estou me esforçando para melhorar. atualmente tenho 17 anos, busco me tornar um profissional em programação </p>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a href='https://openai.com/index/chatgpt/'>Contador</a>
+           
+           <a href='https://www.youtube.com/watch?v=KucUIqs-tMA'>Lista de tarefas</a>
+      </div>
+      <div className='imagem'>
+        <img src="teste.jpeg" rel="teste" height="300px" />
+      </div>
+    </section>  
     </div>
-  );
-}
-
-export default App;
+    )
+  }
